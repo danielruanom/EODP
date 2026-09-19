810c553f67de9d88163048ecbeab6c4e586b794d
Daniel <daniel.ruanom@gmail.com>
2026-09-19 20:15:49 +0200

    Implementation of EODP-ALG-ISM-1035 (smearing MTF)

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -159,7 +159,9 @@ class mtf:
         :param ksmear: Amplitude of low-frequency component for the motion smear MTF in ALT [pixels]
         :return: Smearing MTF
         """
-        #TODO
+        Hsmear = np.zeros((len(fnAlt), ncolumns))
+        for i in range(ncolumns):
+            Hsmear[:, i] = np.sinc(ksmear * fnAlt)
         return Hsmear
 
     def mtfMotion(self, fn2D, kmotion):
